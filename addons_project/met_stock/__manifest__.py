2ebe786ac54da837e075bbb11fd513f0d7e78e79
roger <roger@nexources.com>
2025-11-03 14:05:54 +0300

    [ADD]: stock

---
+++ addons_project/met_stock/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "MET - Stock",
+    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "author": "Nexources",
+    "website": "https://www.nexources.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["met_base", "stock", "purchase"],
+    "data": ["data/ir_cron.xml"],
+}
---
+++ addons_project/met_stock/models/stock_warehouse_orderpoint.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, Command
+from odoo.exceptions import UserError
+
+class StockWarehouseOrderpoint(models.Model):
+    _inherit = "stock.warehouse.orderpoint"
+
+
+    def action_check_orderpoint_and_create_purchase(self):
+        orderpoints = self.search(
+            [
+                ("route_id", "=", 5),
+                ("qty_on_hand", "<", "product_min_qty"),
+                ("trigger", "=", "auto"),
+            ]
+        )
+        vals = {}
+        for order in orderpoints:
+            domain = [("product_tmpl_id", "=", order.product_id.product_tmpl_id.id)]
+            pricelist_id = self.env["product.supplierinfo"].search(
+                domain, order="price asc", limit=1
+            )
+            if pricelist_id.partner_id:
+                if pricelist_id.partner_id not in vals:
+                    vals[pricelist_id.partner_id] = [
+                        Command.create(
+                            {
+                                "product_id": order.product_id.id,
+                                "product_qty": order.qty_to_order,
+                            }
+                        )
+                    ]
+                else:
+                    vals[pricelist_id.partner_id].append(
+                        Command.create(
+                            {
+                                "product_id": order.product_id.id,
+                                "product_qty": order.qty_to_order,
+                            }
+                        )
+                    )
+        for key, value in vals.items():
+            purchase_vals = {"partner_id": key.id, "order_line": value}
+            self.env["purchase.order"].create(purchase_vals)
